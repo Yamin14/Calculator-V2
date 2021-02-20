0c009cb32b426d50ad5e6fde7cd49d1802668010
Yamin Kazmi <77500567+Yamin14@users.noreply.github.com>
2021-02-20 14:27:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Calculator(GridLayout):
 		self.width = 150
 		self.height = 150
 		self.font_size = 70
-		self.x, self.y = 70, 300
+		self.x, self.y = 20, 200
 		
 		self.buttons = []
 		for i in range(10):
@@ -22,10 +22,10 @@ class Calculator(GridLayout):
 				if self.x <= 300:
 					self.x += 150
 				else:
-					self.x = 70
+					self.x = 20
 					self.y += 150
 					
-		for i in range(10, 22):
+		for i in range(10, 26):
 			if i == 10:
 				self.text = "."
 			elif i == 11:
@@ -50,29 +50,45 @@ class Calculator(GridLayout):
 				self.text = "^"
 			elif i == 21:
 				self.text = "!"
-				
+			elif i == 22:
+				self.text = "sin("
+			elif i == 23:
+				self.text = "cos("
+			elif i == 24:
+				self.text = "tan("
+			elif i == 25:
+				self.text = "π"
+		
 			self.buttons.append(Button(text=self.text, background_color=self.bg, color=self.fg, size=(self.width, self.height), font_size=self.font_size))
 			
-		self.buttons[0].pos = (220, 150)
-		self.buttons[10].pos = (70, 150)
-		self.buttons[11].pos = (370, 150)
-		self.buttons[12].pos = (520, 600)
-		self.buttons[13].pos = (520, 450)
-		self.buttons[14].pos = (520, 300)
-		self.buttons[15].pos = (520, 150)
-		self.buttons[16].pos = (70, 750)
-		self.buttons[17].pos = (370, 750)
-		self.buttons[18].pos = (220, 850)
-		self.buttons[19].pos = (370, 850)
-		self.buttons[20].pos = (70, 850)
-		self.buttons[21].pos = (520, 850)
+		self.buttons[0].pos = (170, 50)
+		self.buttons[10].pos = (20, 50)
+		self.buttons[11].pos = (320, 50)
+		self.buttons[12].pos = (470, 500)
+		self.buttons[13].pos = (470, 350)
+		self.buttons[14].pos = (470, 200)
+		self.buttons[15].pos = (470, 50)
+		self.buttons[16].pos = (20, 650)
+		self.buttons[17].pos = (320, 650)
+		self.buttons[18].pos = (170, 750)
+		self.buttons[19].pos = (320, 750)
+		self.buttons[20].pos = (20, 750)
+		self.buttons[21].pos = (470, 750)
+		self.buttons[22].pos = (20, 900)
+		self.buttons[23].pos = (220, 900)
+		self.buttons[24].pos = (420, 900)
+		self.buttons[25].pos = (620, 50)
 		
 		self.buttons[16].size = (300, 100)
 		self.buttons[17].size = (300, 100)
+		self.buttons[22].size = (200, 100)
+		self.buttons[23].size = (200, 100)
+		self.buttons[24].size = (200, 100)
+		self.buttons[25].size = (100, 150)
 		
-		for i in range(22):
+		for i in range(26):
 			self.add_widget(self.buttons[i])
-			if self.buttons[i].text != "=" and self.buttons[i].text != "Delete" and self.buttons[i].text != "Clear" and self.buttons[i].text != "^" and self.buttons[i].text != "÷" and self.buttons[i].text != "×" and self.buttons[i].text != "!":
+			if self.buttons[i].text != "=" and self.buttons[i].text != "Delete" and self.buttons[i].text != "Clear" and self.buttons[i].text != "^" and self.buttons[i].text != "÷" and self.buttons[i].text != "×" and self.buttons[i].text != "!" and self.buttons[i].text != "sin(" and self.buttons[i].text != "cos(" and self.buttons[i].text != "tan(":
 				self.buttons[i].bind(on_press=self.pressed)
 			elif self.buttons[i].text == "Delete":
 				self.buttons[i].bind(on_press=self.delete)
@@ -86,6 +102,12 @@ class Calculator(GridLayout):
 				self.buttons[i].bind(on_press=self.divide)
 			elif self.buttons[i].text == "×":
 				self.buttons[i].bind(on_press=self.multiply)
+			elif self.buttons[i].text == "sin(":
+				self.buttons[i].bind(on_press=self.trig)
+			elif self.buttons[i].text == "cos(":
+				self.buttons[i].bind(on_press=self.trig)
+			elif self.buttons[i].text == "tan(":
+				self.buttons[i].bind(on_press=self.trig)
 			if self.buttons[i].text == "!":
 				self.buttons[i].bind(on_press=self.fact)
 			
@@ -118,7 +140,7 @@ class Calculator(GridLayout):
 		self.num = ""
 		self.i = self.input_label.text.index("!")
 
-		while "+" not in self.num and "-" not in self.num and "*" not in self.num and "/" not in self.num and "(" not in self.num and ")" not in self.num and self.i >= 0:
+		while "+" not in self.num and "-" not in self.num and "*" not in self.num and "/" not in self.num and "(" not in self.num and ")" not in self.num and "!" not in self.num and self.i >= 0:
 			self.i -= 1
 			self.num += self.input_label.text[self.i]
 			
@@ -131,15 +153,26 @@ class Calculator(GridLayout):
 			self.fact_ans = str(factorial(self.num))
 			self.start_pos = self.i + 1
 			self.end_pos = self.input_label.text.index("!")
+			
 		except:
 			pass
+
+	def trig(self, instance):
+		pass
 		
 	def equal(self, instance):
 		try:
 			if "!" not in self.input_label.text:
 				self.expression = self.input_label.text
-			else:
+			elif "!" in self.input_label.text:
 				self.expression = self.input_label.text[:self.start_pos] + self.fact_ans + self.input_label.text[self.end_pos+1:]
+				
+			if "π" in self.input_label.text:
+				pos = 0
+				for i in self.input_label.text:
+					if i == "π":
+						self.expression = self.input_label.text[:pos] + str(pi) + self.input_label.text[pos+1:]
+					pos += 1
 			
 			self.answer = eval(self.expression)
 		except:
